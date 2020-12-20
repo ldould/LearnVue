107bb659793323a0ad781eb7271c58f6e7dee379
ldould <1914017555@qq.com>
2020-12-20 22:50:52 +0800

    vuex

---
+++ 06-learnVuex/01-learnvuex/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="addition">+</button>
+    <button @click="subtraction">-</button>
+
+    <hellow-vuex />
+  </div>
+</template>
+
+<script>
+import HellowVuex from "./components/HellowVuex.vue";
+export default {
+  components: { HellowVuex },
+  name: "App",
+  methods: {
+    addition() {
+      this.$store.commit("increment");
+    },
+    subtraction() {
+      this.$store.commit("decrement");
+    }
+  }
+};
+</script>
+
+<style></style>
