608ff325076b70d38ad5a970da697b0856abf0dc
ldou <1914017555@qq.com>
2020-12-21 17:04:48 +0800

    mutations的使用

--- 06-learnVuex/01-learnvuex/src/App.vue
+++ 06-learnVuex/01-learnvuex/src/App.vue
@@ -4,23 +4,75 @@
     <button @click="addition">+</button>
     <button @click="subtraction">-</button>
 
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStudent">添加学生</button>
+
+    <h2>-------App内容：getters相关信息--------</h2>
+    <h2>{{$store.getters.powerCounter}}</h2>
+    <h2>{{$store.getters.more20stu}}</h2>
+    <h2>{{$store.getters.more20stuLength}}</h2>
+    <h2>{{$store.getters.moreAgeStu(14)}}</h2>
+    <h2>{{$store.state.info}}</h2>
+    <button @click="updateInfo">修改信息</button>
+    <!-- <h2>{{more20}}</h2> -->
+
+
+      <h2>-------helloVuex内容：getters相关信息--------</h2>
     <hellow-vuex />
   </div>
 </template>
 
 <script>
 import HellowVuex from "./components/HellowVuex.vue";
+import { INCREMENT } from './store/mutations-type.js'
 export default {
   components: { HellowVuex },
   name: "App",
   methods: {
     addition() {
-      this.$store.commit("increment");
+      this.$store.commit(INCREMENT);
     },
     subtraction() {
       this.$store.commit("decrement");
+    },
+    //mutations传参
+    addCount(count){
+      //payload：负载
+      //1、普通的提交风格
+    //  this.$store.commit("incrementCount",count);
+
+      //2、特殊的提交风格
+      this.$store.commit({
+        type:'incrementCount',
+        count
+      })
+    },
+    addStudent() {
+      const stu = {id:115,name:'alan',age:35}
+      this.$store.commit("addStudent",stu);
+    },
+    updateInfo(){
+      //  this.$store.dispatch("aUpdateInfo",{
+      //    message:'我是playload',
+      //    success:() => {
+      //      console.log('里面已经完成了')
+      //    }
+      //  });
+
+      this.$store.dispatch("aUpdateInfo",'我是携带的信息')
+      .then(res => {
+        console.log('里面已经完成了');
+        console.log(res)
+      })
     }
-  }
+
+  },
+  // computed:{
+  //   more20(){
+  //     return this.$store.state.students.filter(s => s.age>=20)
+  //   }
+  // }
 };
 </script>
 
--- 06-learnVuex/01-learnvuex/src/store/index.js
+++ 06-learnVuex/01-learnvuex/src/store/index.js
@@ -1,22 +1,96 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { INCREMENT } from './mutations-type'
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    counter: 100
+    counter: 100,
+    students: [
+      { id: 110, name: 'hh', age: 14 },
+      { id: 112, name: 'kk', age: 21 },
+      { id: 113, name: 'oo', age: 18 },
+      { id: 114, name: 'gg', age: 30 },
+    ],
+    info: {
+      id: 116,
+      name: 'lisa',
+      age: 19
+    }
   },
   mutations: {
-    increment(state) {
+    [INCREMENT](state) {
       state.counter++;
     },
     decrement(state) {
       state.counter--;
+    },
+    //mutations获取参数
+    // incrementCount(state, count) {
+    //   state.counter += count
+    // },
+    incrementCount(state, playload) {
+      console.log(playload)//打印出一个对象
+      state.counter += playload.count
+    },
+    addStudent(state, stu) {
+      state.students.push(stu)
+    },
+    updateInfo(state) {
+      state.info.name = 'xiaoming'
+      //mutations里的方法必须是同步方法，主要原因是当我们使用devtools时，devtools可以帮我们捕捉mutation的快照，
+      //但是如果是异步操作，那么devtools将不能很好的跟踪这个操作什么时候会被完成
+      // setTimeout(() => {
+      //   state.info.name = 'xiaoming'
+      // }, 1000)
+      // state.info.address = '广东'
+      // state.info['address'] = '广州'
+      // // delete state.info.age
+      // Vue.set(state.info, 'sex', '男')
+      // Vue.delete(state.info, 'age')
     }
   },
-  actions: {},
-  getters: {},
+  actions: {
+    // aUpdateInfo(context, playload) {
+
+    //   setTimeout(() => {
+    //     context.commit('updateInfo')
+    //     console.log(playload.message)
+    //     playload.success()
+    //   }, 1000)
+    // }
+
+    aUpdateInfo(context, playload) {
+
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          context.commit('updateInfo')
+          console.log(playload)
+          resolve('111')
+        }, 1000)
+      })
+    }
+  },
+  getters: {
+    powerCounter(state) {
+      return state.counter * state.counter
+    },
+    more20stu(state) {
+      return state.students.filter(s => s.age > 20)
+    },
+    more20stuLength(state, getters) {
+      return getters.more20stu.length
+    },
+    moreAgeStu(state) {
+      // return function (age) {
+      //   return state.students.filter(s => s.age > age)
+      // }
+      return age => {
+        return state.students.filter(s => s.age > age)
+      }
+    }
+  },
   modules: {}
 });
 
