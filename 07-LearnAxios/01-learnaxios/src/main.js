814012ec054db3701b6a61c9d0a1cc7b97c499a2
ldou <1914017555@qq.com>
2020-12-23 17:33:59 +0800

    axios的配置、实例和模块封装、拦截器

--- 07-LearnAxios/01-learnaxios/src/main.js
+++ 07-LearnAxios/01-learnaxios/src/main.js
@@ -30,15 +30,94 @@ new Vue({
 // })
 
 //2.axios发送并发请求
-axios.all([axios({
-  url: 'http://123.207.32.32:8000/home/multidata'
-}), axios({
-  url: 'http://123.207.32.32:8000/home/data',
-  //专门针对get请求的参数拼接
-  params: {
-    type: 'sell',
-    page: 2
-  }
-})]).then(results => {
-  console.log(results)
+// axios.all([axios({
+//   url: 'http://123.207.32.32:8000/home/multidata'
+// }), axios({
+//   url: 'http://123.207.32.32:8000/home/data',
+//   //专门针对get请求的参数拼接
+//   params: {
+//     type: 'sell',
+//     page: 2
+//   }
+// })]).then(results => {
+//   console.log(results)
+//   console.log(results[0])
+//   console.log(results[1])
+// })
+
+
+//3.使用全局的axios和对应的配置在进行网络请求
+// axios.defaults.baseURL = 'http://152.136.185.210:7878/api/m5'
+// axios.defaults.timeout = 5000
+
+// axios.all([axios({
+//   url: '/home/multidata'
+// }), axios({
+//   url: '/home/data',
+//   //专门针对get请求的参数拼接
+//   params: {
+//     type: 'sell',
+//     page: 2
+//   }
+// })]).then(axios.spread((res1, res2) => {
+//   console.log(res1)
+//   console.log(res2)
+// }))
+
+//4.创建对应的axios的实例
+// const instance1 = axios.create({
+//   baseURL: 'http://152.136.185.210:7878/api/m5',
+//   timeout: 5000
+// })
+// instance1({
+//   url: '/home/multidata'
+// }).then(res => {
+//   console.log(res)
+// })
+// instance1({
+//   url: '/home/data',
+//   params: {
+//     type: 'sell',
+//     page: 2
+//   }
+// }).then(res => {
+//   console.log(res)
+// })
+
+// const instance2 = axios.create({
+//   baseURL: 'http://152.136.185.210:7878',
+//   timeout: 3000,
+//   // headers:{}
+// })
+
+//5.封装request模块
+import { request } from './network/request'
+
+// request({
+//   url: '/home/multidata',
+// }, res => {
+//   console.log(res)
+// }, err => {
+//   console.log(err)
+// })
+
+// request({
+//   baseConfig: {
+//     url: '/home/multidata'
+//   },
+//   success: function (res) {
+//     console.log(res)
+//   },
+//   failure: function (err) {
+//     console.log(err)
+//   }
+// })
+
+
+request({
+  url: '/home/multidata',
+}).then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
 })
