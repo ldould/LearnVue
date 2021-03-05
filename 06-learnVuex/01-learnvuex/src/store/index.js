a744ac611b5ce63bee300b7975b5acb18f2b5eb4
ldould <1914017555@qq.com>
2021-03-05 22:15:14 +0800

    03/05

--- 06-learnVuex/01-learnvuex/src/store/index.js
+++ 06-learnVuex/01-learnvuex/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { INCREMENT } from './mutations-type'
+import { INCREMENT } from "./mutations-type";
 
 Vue.use(Vuex);
 
@@ -8,14 +8,14 @@ const store = new Vuex.Store({
   state: {
     counter: 100,
     students: [
-      { id: 110, name: 'hh', age: 14 },
-      { id: 112, name: 'kk', age: 21 },
-      { id: 113, name: 'oo', age: 18 },
-      { id: 114, name: 'gg', age: 30 },
+      { id: 110, name: "hh", age: 14 },
+      { id: 112, name: "kk", age: 21 },
+      { id: 113, name: "oo", age: 18 },
+      { id: 114, name: "gg", age: 30 }
     ],
     info: {
       id: 116,
-      name: 'lisa',
+      name: "lisa",
       age: 19
     }
   },
@@ -31,14 +31,14 @@ const store = new Vuex.Store({
     //   state.counter += count
     // },
     incrementCount(state, playload) {
-      console.log(playload)//打印出一个对象
-      state.counter += playload.count
+      console.log(playload); //打印出一个对象
+      state.counter += playload.count;
     },
     addStudent(state, stu) {
-      state.students.push(stu)
+      state.students.push(stu);
     },
     updateInfo(state) {
-      state.info.name = 'xiaoming'
+      state.info.name = "xiaoming";
       //mutations里的方法必须是同步方法，主要原因是当我们使用devtools时，devtools可以帮我们捕捉mutation的快照，
       //但是如果是异步操作，那么devtools将不能很好的跟踪这个操作什么时候会被完成
       // setTimeout(() => {
@@ -62,33 +62,32 @@ const store = new Vuex.Store({
     // }
 
     aUpdateInfo(context, playload) {
-
       return new Promise((resolve, reject) => {
         setTimeout(() => {
-          context.commit('updateInfo')
-          console.log(playload)
-          resolve('111')
-        }, 1000)
-      })
+          context.commit("updateInfo");
+          console.log(playload);
+          resolve("111");
+        }, 1000);
+      });
     }
   },
   getters: {
     powerCounter(state) {
-      return state.counter * state.counter
+      return state.counter * state.counter;
     },
     more20stu(state) {
-      return state.students.filter(s => s.age > 20)
+      return state.students.filter(s => s.age > 20);
     },
     more20stuLength(state, getters) {
-      return getters.more20stu.length
+      return getters.more20stu.length;
     },
     moreAgeStu(state) {
       // return function (age) {
       //   return state.students.filter(s => s.age > age)
       // }
       return age => {
-        return state.students.filter(s => s.age > age)
-      }
+        return state.students.filter(s => s.age > age);
+      };
     }
   },
   modules: {}
